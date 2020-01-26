670d5c1607b7e1ff97a872333486ac9c6bf45e33
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-01-26 22:59:55 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,7 +11,7 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
   
   the_manifest = hash
-  hash = {""
+  hash = {"whale bone corsets"=>5,""
 
 
 end
